22b0aa7a26811a9948c31607e03139bb3c2449a9
Maria Mesimeri <mmesim@gmail.com>
2023-01-30 15:04:16 +0100

    Update Docs

---
+++ kagan.angle/README.md
@@ -0,0 +1,9 @@
+### How TO
+
+- Define the input file [line  8]
+
+  The input file is the output  of hybridMT after resampling(!)
+
+* run kagan_angle.m 
+
+  It should create three histograms. 
--- kagan.angle/kagan_angle.m
+++ kagan.angle/kagan_angle.m
@@ -13,6 +13,7 @@ N=length(Solution{1}.dc.F1(:,1))-1;
 rot_dc=NaN(N-1,1);rot_dev=NaN(N-1,1);rot_full=NaN(N-1,1);
 theta_dc=NaN(N-1,1);theta_dev=NaN(N-1,1);theta_full=NaN(N-1,1);
 phi_dc=NaN(N-1,1);phi_dev=NaN(N-1,1);phi_full=NaN(N-1,1);
+
 %% Double Couple
 for i=2:N
 [rot_dc(i-1,1),theta_dc(i-1,1),phi_dc(i-1,1)]=kagan(Solution{1}.dc.F1(1,1:3),Solution{1}.dc.F1(i,1:3));
